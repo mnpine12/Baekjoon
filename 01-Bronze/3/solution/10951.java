0439d15a2b35e01fbff2fd62adefd025627f253b
mnpine12 <49507932+mnpine12@users.noreply.github.com>
2022-01-05 20:16:28 +0900

    Create 10951.java
    2022-01-05 백준 문제 풀이 저장

---
+++ 01-Bronze/3/solution/10951.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String input = "";
+        int sum;
+
+        while((input = br.readLine()) != null){
+            sum = 0;
+            String[] array = input.split(" ");
+            for(int i=0; i<array.length; i++){
+                sum += Integer.parseInt(array[i]);
+            }
+            bw.write(sum + "\n");
+        }
+        bw.close();
+    }
+}
