f5dfde36b0088001dd3fd7d72d6e0d9030c49cc8
mnpine12 <49507932+mnpine12@users.noreply.github.com>
2022-01-05 19:53:22 +0900

    Create 10869.java
    2022-01-05 백준 문제 풀이 저장

---
+++ 01-Bronze/5/solution/10869.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        
+        a = sc.nextInt();
+        b = sc.nextInt();
+        
+        if(a >= 1 && b <= 10000){
+            System.out.println(a+b);
+            System.out.println(a-b);
+            System.out.println(a*b);
+            System.out.println(a/b);
+            System.out.println(a%b);
+        }
+    }
+}
