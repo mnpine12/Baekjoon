d5bdfde9c6d71f263c7690aa767c8976a8dfb832
mnpine12 <49507932+mnpine12@users.noreply.github.com>
2022-01-05 20:26:00 +0900

    Create 10952.java
    2022-01-05 백준 문제 풀이 저장

---
+++ 01-Bronze/3/solution/10952.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int a,b;
+        
+        st = new StringTokenizer(br.readLine());
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+
+        while(a>0 && b>0){
+            bw.write(a + b + "\n");
+            st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+        }
+        bw.close();
+    }
+}
