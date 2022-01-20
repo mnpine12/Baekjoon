dd8cae85ade2f87e4438b1f2feb5db5866bb0c31
mnpine12 <49507932+mnpine12@users.noreply.github.com>
2022-01-20 16:03:17 +0900

    Create 11651.java
    20220120 - 백준 문제 풀이 저장

---
+++ 02-Silver/5/11651.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Point> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            Point p = new Point(x, y);
+            list.add(p);
+        }
+
+        Collections.sort(list, new Comparator<Point>() {
+            @Override
+            public int compare(Point o1, Point o2) {
+                if(o1.getY() == o2.getY()){
+                    return o1.getX() - o2.getX();
+                }
+                return o1.getY() - o2.getY();
+            }
+        });
+
+        for(Point point : list){
+            System.out.println(point.getX() + " " + point.getY());
+        }
+    }
+}
+
+class Point <Point> {
+    int x;
+    int y;
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+}
